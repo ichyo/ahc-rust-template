e4bc21819095545c0b2f3dc3dadcf5b1320b944a
ichyo <ichyo00@gmail.com>
2023-09-24 14:58:05 +0900

    Fix submit.sh

--- submit.sh
+++ submit.sh
@@ -3,6 +3,7 @@
 set -eu
 
 CONTEST_ID="" # e.g., ahc020
+TASK_ID="$CONTEST_ID"_a
 
 if [[ $CONTEST_ID == "" ]]; then
     echo "Error: Update CONTEST_ID in this script"
@@ -14,10 +15,10 @@ cargo check
 tmpfile=$(mktemp /tmp/submission.XXXXXX.rs)
 
 echo "Writing submission file to $tmpfile"
-cargo equip --bin submission --exclude-atcoder-crates --no-check > $tmpfile
+cargo equip --bin submission --exclude-atcoder-202301-crates --no-check > $tmpfile
 
 echo "Submitting to $CONTEST_ID"
-oj submit "https://atcoder.jp/contests/${CONTEST_ID}/tasks/${CONTEST_ID}_a" $tmpfile -y
+oj submit "https://atcoder.jp/contests/${CONTEST_ID}/tasks/${TASK_ID}" $tmpfile -y
 
 
 
