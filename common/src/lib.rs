ab5e8a4b8346501714f67a4149f1bfcd561fc854
ichyo <ichyo00@gmail.com>
2023-09-24 14:49:59 +0900

    Move stdin read to common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,4 @@
+use proconio::input;
 use rand::prelude::*;
 
 pub struct ProblemInput {
@@ -17,6 +18,27 @@ pub trait Environment {
 
     fn receive_input(&mut self) -> ProblemInput;
     fn send_output(&mut self, output: ProblemOutput);
+}
+
+pub struct Atcoder {}
+
+impl Environment for Atcoder {
+    fn receive_input(&mut self) -> ProblemInput {
+        // Write parser of text input here
+
+        input! {
+            a: i64,
+            b: i64,
+        }
+
+        ProblemInput { a, b }
+    }
+
+    fn send_output(&mut self, output: ProblemOutput) {
+        // Write print of text output here
+
+        println!("{}", output.result);
+    }
 }
 
 pub fn solve<E: Environment>(environment: &mut E) {
--- submission/src/main.rs
+++ submission/src/main.rs
@@ -1,27 +1,4 @@
-use proconio::input;
-
 use common::*;
-
-struct Atcoder {}
-
-impl Environment for Atcoder {
-    fn receive_input(&mut self) -> ProblemInput {
-        // Write parser of text input here
-
-        input! {
-            a: i64,
-            b: i64,
-        }
-
-        ProblemInput { a, b }
-    }
-
-    fn send_output(&mut self, output: ProblemOutput) {
-        // Write print of text output here
-
-        println!("{}", output.result);
-    }
-}
 
 fn main() {
     solve(&mut Atcoder {});
