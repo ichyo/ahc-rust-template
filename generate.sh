369a182f87f980352230d193df6dc36c7ec5b2a0
ichyo <ichyo00@gmail.com>
2024-11-10 14:58:22 +0900

    Remove comments

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo +1.76 equip --bin submission --exclude-atcoder-202301-crates --no-check
+cargo +1.76 equip --bin submission --exclude-atcoder-202301-crates --no-check --remove docs --remove comments
